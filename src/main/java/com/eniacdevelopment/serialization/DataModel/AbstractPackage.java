fa41653de5ce0e4b3f981096f0c17b265697c586
Lars Gardien <larsgardien@live.nl>
2016-10-12 11:34:53 +0200

    Removed abundant file

--- src/main/java/com/eniacdevelopment/serialization/DataModel/AbstractPackage.java
+++ src/main/java/com/eniacdevelopment/serialization/DataModel/AbstractPackage.java
@@ -7,12 +7,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 /**
  * Created by larsg on 10/5/2016.
  */
-//@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.EXTERNAL_PROPERTY, property="type", visible = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property="type", visible=true)
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = EventNotification.class, name=PackageType.Constants.NOTIFICATION),
-
+        @JsonSubTypes.Type(value = EventNotification.class, name=PackageType.Constants.NOTIFICATION)
         //@JsonSubTypes.Type(value = Request.class, name=PackageType.Constants.REQUEST)
 })
 public abstract class AbstractPackage {
