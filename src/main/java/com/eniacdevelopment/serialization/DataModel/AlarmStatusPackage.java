9ceaa0ca92a97f7354432aa70f2ed2b03c4784a2
Lars Gardien <larsgardien@live.nl>
2016-10-13 19:15:12 +0200

    Renamed and added some classes

---
+++ src/main/java/com/eniacdevelopment/serialization/DataModel/AlarmStatusPackage.java
@@ -0,0 +1,7 @@
+package com.eniacdevelopment.serialization.DataModel;
+
+/**
+ * Created by larsg on 10/13/2016.
+ */
+public class AlarmStatusPackage {
+}
---
+++ src/main/java/com/eniacdevelopment/serialization/DataModel/User/CreateUserPackage.java
@@ -0,0 +1,7 @@
+package com.eniacdevelopment.serialization.DataModel.User;
+
+/**
+ * Created by larsg on 10/13/2016.
+ */
+public class CreateUserPackage {
+}
