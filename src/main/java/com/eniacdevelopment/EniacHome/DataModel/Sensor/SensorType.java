aa9040b7cb63d95a8fa710b862a212e7fdf2444a
Lars Gardien <larsgardien@live.nl>
2017-01-15 16:58:18 +0100

    Fixed Temperature typo

--- src/main/java/com/eniacdevelopment/EniacHome/DataModel/Sensor/SensorType.java
+++ src/main/java/com/eniacdevelopment/EniacHome/DataModel/Sensor/SensorType.java
@@ -4,7 +4,7 @@ package com.eniacdevelopment.EniacHome.DataModel.Sensor;
  * Created by larsg on 1/4/2017.
  */
 public enum SensorType {
-    TemeratureSensor,
+    TemperatureSensor,
     SmokeSensor,
     ContactSensor,
     InfraredSensor,
