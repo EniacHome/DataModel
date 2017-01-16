7b94ecafca46952d8e1f14b9602bc7e90e299726
Lars Gardien <larsgardien@live.nl>
2017-01-16 20:08:08 +0100

    UserRole string fix

--- src/main/java/com/eniacdevelopment/EniacHome/DataModel/User/UserRole.java
+++ src/main/java/com/eniacdevelopment/EniacHome/DataModel/User/UserRole.java
@@ -4,6 +4,9 @@ package com.eniacdevelopment.EniacHome.DataModel.User;
  * Created by larsg on 12/16/2016.
  */
 public enum UserRole {
-    Admin,
-    User
+    User,
+    Admin;
+
+    public static final String USER = "User";
+    public static final String ADMIN = "Admin";
 }
